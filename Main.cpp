7068cf813681de1c41449cdff4d6f4e1e3913da2
dangutie <43732574+dangutie@users.noreply.github.com>
2018-09-30 18:53:47 -0400

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -2,7 +2,7 @@
   File:          Main.cc
   Description:   
 
-  Authors:       Daniel Gutierrez, Pedro Garcia.
+  Authors:       Daniel Gutierrez, Pedro Garcia
   Class:         Security In Computing
   Date:          September 2018
 ***************************************************************/
@@ -10,12 +10,26 @@
 
 #include <cstdlib>
 #include <iostream>
+#include <fstream>
 #include <string>
+#include "AES.h"
 
 using namespace std;
 
 int main()
 {
+	string Password;
+	string FileName;
+
+	cout << "Enter the name of the file to be read..." << endl;
+	cin >> FileName;
+	cout << "Enter the 16 character password to be used as the encryption key..." << endl;
+	cin >> Password;
+
+	ifstream MyFile;
+
+	MyFile.open(FileName);
+
 
 
 	return 0;
