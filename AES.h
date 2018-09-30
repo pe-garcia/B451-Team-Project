a4c97e635df7035dd364a90e82c38e6d0f78e472
dangutie <43732574+dangutie@users.noreply.github.com>
2018-09-30 18:21:20 -0400

    Add files via upload

---
+++ AES.h
@@ -0,0 +1,13 @@
+/***************************************************************
+  File:          AES.h
+  Description:   
+
+  Authors:        Daniel Gutierrez, Pedro Garcia
+  Class:          Security in Computing
+  Date:           September 2018
+****************************************************************/
+
+#ifndef AES_H
+#define AES_H
+
+#endif
---
+++ Main.cpp
@@ -0,0 +1,22 @@
+/**************************************************************
+  File:          Main.cc
+  Description:   
+
+  Authors:       Daniel Gutierrez, Pedro Garcia.
+  Class:         Security In Computing
+  Date:          September 2018
+***************************************************************/
+
+
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+
+
+	return 0;
+}
